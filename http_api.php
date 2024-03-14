7e9b74c9d0da8110f557758e711a34d909f689af
Adam Zieliński <adam@adamziel.com>
2024-03-14 13:31:14 +0100

    Experiment with parallelized sequential processing

--- http_api.php
+++ http_api.php
@@ -2,9 +2,11 @@
 
 require __DIR__ . '/src/WordPress/Streams/StreamPeeker.php';
 require __DIR__ . '/src/WordPress/Streams/StreamPeekerContext.php';
+require __DIR__ . '/src/WordPress/Blueprints/Resources/ResourceMap.php';
 
 use WordPress\Streams\StreamPeeker;
 use WordPress\Streams\StreamPeekerContext;
+use WordPress\Blueprints\Resources\ResourceMap;
 
 function streams_http_open_nonblocking( $urls ) {
 	$streams = [];
@@ -139,6 +141,13 @@ function parse_headers( string $headers ) {
 
 function handle_response_headers( array $headers ) {
 	// Assume it's alright
+	if ( $headers['status']['code'] > 399 || $headers['status']['code'] < 200 ) {
+		throw new Exception( "Failed to download file" );
+	}
+	if ( isset( $headers['headers']['location'] ) ) {
+		// @TODO: Handle redirects
+		throw new Exception( "HTTP redirects are not supported yet" );
+	}
 }
 
 function prepare_request_bytes( $url ) {
@@ -150,10 +159,11 @@ GET $path HTTP/1.1
 Host: $host
 User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
-Accept-Encoding: gzip, deflate, br
 Accept-Language: en-US,en;q=0.9
 Connection: keep-alive
 REQUEST;
+
+	// @TODO: Add support for Accept-Encoding: gzip
 
 	return str_replace( "\n", "\r\n", $request ) . "\r\n\r\n";
 }
@@ -179,7 +189,6 @@ function streams_http_response_await_headers( $streams ) {
 
 	foreach ( $headers as $k => $header ) {
 		$headers[ $k ] = parse_headers( $header );
-		handle_response_headers( $headers[ $k ] );
 	}
 
 	return $headers;
@@ -213,25 +222,113 @@ function monitor_progress( $stream, $contentLength, $onProgress ) {
 	);
 }
 
-$streams = streams_http_open_nonblocking( [
+class AsyncStreamsCollection {
+	protected $nb_streams = 0;
+	protected array $streams;
+	protected array $buffers;
+
+	public function __construct( $streams ) {
+		foreach ( $streams as $stream ) {
+			$this->add_stream( $stream );
+		}
+	}
+
+	public function add_stream( $stream ) {
+		$key = $this->nb_streams ++;
+		$this->streams[ $key ] = $stream;
+		$this->buffers[ $key ] = '';
+	}
+
+	public function read_bytes( $stream, $length ) {
+		$key = array_search( $stream, $this->streams );
+		if ( false === $key ) {
+			return false;
+		}
+
+		while ( true ) {
+			if ( strlen( $this->buffers[ $key ] ) >= $length ) {
+				$buffered = substr( $this->buffers[ $key ], 0, $length );
+				$this->buffers[ $key ] = substr( $this->buffers[ $key ], $length );
+
+				return $buffered;
+			} elseif ( feof( $stream ) ) {
+				$buffered = $this->buffers[ $key ];
+				unset( $this->buffers[ $key ] );
+				unset( $this->streams[ $key ] );
+
+				return strlen( $buffered ) ? $buffered : false;
+			}
+			$remaining_length = $length - strlen( $this->buffers[ $key ] );
+			$bytes = sockets_http_response_await_bytes( $this->streams, $remaining_length );
+			foreach ( $bytes as $k => $chunk ) {
+				$this->buffers[ $k ] .= $chunk;
+			}
+		}
+	}
+}
+
+function start_downloads( $urls, $onProgress ) {
+	$streams = streams_http_open_nonblocking( $urls );
+	streams_http_requests_send( $streams );
+
+	$stream_headers = streams_http_response_await_headers( $streams );
+	foreach ( $stream_headers as $k => $headers ) {
+		handle_response_headers( $headers );
+	}
+
+	return streams_monitor_progress(
+		$streams,
+		$stream_headers,
+		$onProgress
+	);
+}
+
+$streams = start_downloads( [
 	"https://downloads.wordpress.org/plugin/gutenberg.17.9.0.zip",
 	"https://downloads.wordpress.org/plugin/woocommerce.8.6.1.zip",
 	"https://downloads.wordpress.org/plugin/hello-dolly.1.7.3.zip",
-] );
-streams_http_requests_send( $streams );
+], function ( $downloaded, $total ) {
+	echo "Downloaded: $downloaded / $total\n";
+} );
 
-$headers = streams_http_response_await_headers( $streams );
-$streams = streams_monitor_progress(
-	$streams,
-	$headers,
-	function ( $downloaded, $total ) {
-		echo "onProgress callback " . $downloaded . "/$total\n";
-	}
-);
-print_r( $headers );
+// Non-blocking parallel processing – the fastest method.
+//while ( $results = sockets_http_response_await_bytes( $streams, 8096 ) ) {
+//	foreach ( $results as $k => $chunk ) {
+//		file_put_contents( 'output' . $k . '.zip', $chunk, FILE_APPEND );
+//	}
+//}
 
-while ( $results = sockets_http_response_await_bytes( $streams, 1024 ) ) {
-	foreach ( $results as $k => $chunk ) {
-		file_put_contents( 'output' . $k . '.zip', $chunk, FILE_APPEND );
+// Blocking sequential processing – the slowest method.
+//foreach ( $streams as $k => $stream ) {
+//	stream_set_blocking( $stream, 1 );
+//	file_put_contents( 'output' . $k . '.zip', stream_get_contents( $stream ) );
+//}
+
+// Non-blocking parallelized sequential processing – the second fastest method.
+// Polls all the streams when any stream is read.
+$collection = new AsyncStreamsCollection( $streams );
+
+// Download one file
+while ( false !== ( $bytes = $collection->read_bytes( $streams[0], 8096 ) ) ) {
+	file_put_contents( 'output0.zip', $bytes, FILE_APPEND );
+}
+
+// Start more downloads
+$more_streams = start_downloads( [
+	"https://downloads.wordpress.org/plugin/akismet.4.1.12.zip",
+	"https://downloads.wordpress.org/plugin/jetpack.10.0.zip",
+	"https://downloads.wordpress.org/plugin/wordpress-seo.17.9.zip",
+], function ( $downloaded, $total ) {
+	echo "Downloaded: $downloaded / $total\n";
+} );
+foreach ( $more_streams as $k => $stream ) {
+	$collection->add_stream( $stream );
+}
+
+// Download the rest of the files
+$all_streams = array_merge( $streams, $more_streams );
+foreach ( $all_streams as $k => $stream ) {
+	while ( false !== ( $bytes = $collection->read_bytes( $stream, 8096 ) ) ) {
+		file_put_contents( 'output' . $k . '.zip', $bytes, FILE_APPEND );
 	}
 }
