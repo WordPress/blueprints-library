36296f9ee8bbfc4f945943895c0e54d1b1614ba7
Adam Zieliński <adam@adamziel.com>
2024-03-14 12:42:21 +0100

    Non-blocking SSL handshake and HTTP requests

--- http_api.php
+++ http_api.php
@@ -6,93 +6,83 @@ require __DIR__ . '/src/WordPress/Streams/StreamPeekerContext.php';
 use WordPress\Streams\StreamPeeker;
 use WordPress\Streams\StreamPeekerContext;
 
-function stream_notification_callback(
-	$notification_code,
-	$severity,
-	$message,
-	$message_code,
-	$bytes_transferred,
-	$bytes_max
-) {
-	var_dump( $notification_code );
-	static $filesize = null;
-
-	switch ( $notification_code ) {
-		case STREAM_NOTIFY_RESOLVE:
-		case STREAM_NOTIFY_AUTH_REQUIRED:
-		case STREAM_NOTIFY_COMPLETED:
-		case STREAM_NOTIFY_FAILURE:
-		case STREAM_NOTIFY_AUTH_RESULT:
-			/* Ignore */
-			break;
-
-		case STREAM_NOTIFY_REDIRECTED:
-			echo "Being redirected to: ", $message, "\n";
-			break;
-
-		case STREAM_NOTIFY_CONNECT:
-			echo "Connected...\n";
-			break;
-
-		case STREAM_NOTIFY_FILE_SIZE_IS:
-			$filesize = $bytes_max;
-			echo "Filesize: ", $filesize, "\n";
-			break;
-
-		case STREAM_NOTIFY_MIME_TYPE_IS:
-			echo "Mime-type: ", $message, "\n";
-			break;
-
-		case STREAM_NOTIFY_PROGRESS:
-			if ( $bytes_transferred > 0 ) {
-				if ( ! isset( $filesize ) ) {
-					printf( "\rUnknown filesize.. %2d kb done..", $bytes_transferred / 1024 );
-				} else {
-					$length = (int) ( ( $bytes_transferred / $filesize ) * 100 );
-					printf( "\r[%-100s] %d%% (%2d/%2d kb)",
-						str_repeat( "=", $length ) . ">",
-						$length,
-						( $bytes_transferred / 1024 ),
-						$filesize / 1024 );
-				}
-			}
-			break;
+function open_http_streams( $urls ) {
+	$streams = [];
+	foreach ( $urls as $url ) {
+		$streams[] = open_http_stream( $url );
 	}
+
+	return $streams;
 }
 
-function open_nonblocking_socket( $url ) {
+function open_http_stream( $url ) {
+	$parts = parse_url( $url );
+	$scheme = $parts['scheme'];
+	if ( ! in_array( $scheme, [ 'http', 'https' ] ) ) {
+		throw new InvalidArgumentException( 'Invalid scheme – only http:// and https:// URLs are supported' );
+	}
+
+	$port = $parts['port'] ?? ( $scheme === 'https' ? 443 : 80 );
+	$host = $parts['host'];
+
+	// Create stream context
+	$context = stream_context_create(
+		[
+			'socket' => [
+				'isSsl'       => $scheme === 'https',
+				'originalUrl' => $url,
+				'socketUrl'   => 'tcp://' . $host . ':' . $port,
+			],
+		]
+	);
+	var_dump( 'tcp://' . $host . ':' . $port );
 	$stream = stream_socket_client(
-		$url,
+		'tcp://' . $host . ':' . $port,
 		$errno,
 		$errstr,
 		30,
-		STREAM_CLIENT_CONNECT | STREAM_CLIENT_ASYNC_CONNECT
+		STREAM_CLIENT_CONNECT | STREAM_CLIENT_ASYNC_CONNECT,
+		$context
 	);
 	if ( $stream === false ) {
 		throw new Exception( 'Unable to open stream' );
 	}
 
-	stream_socket_enable_crypto( $stream, true, STREAM_CRYPTO_METHOD_TLSv1_2_CLIENT );
 	stream_set_blocking( $stream, 0 );
 
 	return $stream;
 }
 
-function blocking_read_from_stream( $stream, $length, $timeout_microseconds = 500000 ) {
-	$read = [ $stream ];
-	$write = [];
-	$except = null;
-	$ready = @stream_select( $read, $write, $except, 0, $timeout_microseconds );
+function send_http_requests( $streams ) {
+	$read = $except = null;
+	$remaining_streams = $streams;
+	while ( count( $remaining_streams ) ) {
+		$write = $remaining_streams;
+		$ready = @stream_select( $read, $write, $except, 0, 500000 );
+		if ( $ready === false ) {
+			$error = error_get_last();
+			throw new Exception( "Error: " . $error['message'] );
+		} elseif ( $ready <= 0 ) {
+			throw new Exception( "stream_select timed out" );
+		}
 
-	if ( $ready === false ) {
-		$error = error_get_last();
-		throw new Exception( "Error: " . $error['message'] );
-	} elseif ( $ready > 0 ) {
-		return fread( $stream, $length );
-	} else {
-		throw new Exception( "stream_select timed out" );
+		foreach ( $write as $k => $stream ) {
+			$enabled_crypto = stream_socket_enable_crypto( $stream, true, STREAM_CRYPTO_METHOD_TLSv1_2_CLIENT );
+			if ( false === $enabled_crypto ) {
+				throw new Exception( "Failed to enable crypto" );
+			} elseif ( 0 === $enabled_crypto ) {
+				// Wait for the handshake to complete
+			} else {
+				// SSL handshake complete, send the request headers
+				$context = stream_context_get_options( $stream );
+				$request = prepare_request_bytes( $context['socket']['originalUrl'] );
+				fwrite( $stream, $request );
+				unset( $remaining_streams[ $k ] );
+			}
+		}
 	}
 }
+
 
 function poll_streams( $streams, $length, $timeout_microseconds = 500000 ) {
 	$active_streams = array_filter( $streams, function ( $stream ) {
@@ -122,27 +112,6 @@ function poll_streams( $streams, $length, $timeout_microseconds = 500000 ) {
 	return $chunks;
 }
 
-function blocking_stream_get_contents( $stream, $timeout_microseconds = 500000 ) {
-	$contents = '';
-	while ( $bytes = poll_streams( [ $stream ], 1024, $timeout_microseconds ) ) {
-		$contents .= $bytes[0];
-	}
-
-	return $contents;
-}
-
-function blocking_read_headers( $stream ) {
-	$headers = '';
-	while ( true ) {
-		$byte = blocking_read_from_stream( $stream, 1 );
-		$headers .= $byte;
-		if ( substr( $headers, - 4 ) === "\r\n\r\n" ) {
-			break;
-		}
-	}
-
-	return $headers;
-}
 
 function parse_headers( string $headers ) {
 	$lines = explode( "\r\n", $headers );
@@ -163,39 +132,13 @@ function parse_headers( string $headers ) {
 	}
 
 	return [
-		'status' => $status,
+		'status'  => $status,
 		'headers' => $headers,
 	];
 }
 
 function handle_response_headers( array $headers ) {
 	// Assume it's alright
-}
-
-function start_downloads( array $urls, $timeout_microseconds = 500000 ) {
-	$streams = [];
-	foreach ( $urls as $url ) {
-		$streams[] = start_download( $url );
-	}
-
-	foreach ( $streams as $k => $stream ) {
-		$request = prepare_request_bytes( $urls[ $k ] );
-		// This shouldn't block in theory:
-		fwrite( $stream, $request );
-		// This shouldn't block, but I think it sends raw unencrypted data
-		// and ignores the SSL context:
-		// stream_socket_sendto( $stream, $request, STREAM_OOB );
-	}
-
-	return $streams;
-}
-
-function start_download( string $url ) {
-	// Split $url into $host and $path, $path should contain the query string
-	$parts = parse_url( $url );
-	$host = $parts['host'];
-
-	return open_nonblocking_socket( 'tcp://' . $host . ':443' );
 }
 
 function prepare_request_bytes( $url ) {
@@ -220,7 +163,7 @@ function consume_headers( $streams ) {
 	foreach ( $streams as $k => $stream ) {
 		$headers[ $k ] = '';
 	}
-	$remaining_streams = [ ...$streams ];
+	$remaining_streams = $streams;
 	while ( true ) {
 		$bytes = poll_streams( $remaining_streams, 1 );
 		if ( false === $bytes ) {
@@ -255,13 +198,12 @@ function monitor_progress( $stream, $contentLength, $onProgress ) {
 	);
 }
 
-$streams = start_downloads(
-	[
-		"https://downloads.wordpress.org/plugin/gutenberg.17.9.0.zip",
-		"https://downloads.wordpress.org/plugin/woocommerce.8.6.1.zip",
-		"https://downloads.wordpress.org/plugin/hello-dolly.1.7.3.zip",
-	]
-);
+$streams = open_http_streams( [
+	"https://downloads.wordpress.org/plugin/gutenberg.17.9.0.zip",
+	"https://downloads.wordpress.org/plugin/woocommerce.8.6.1.zip",
+	"https://downloads.wordpress.org/plugin/hello-dolly.1.7.3.zip",
+] );
+send_http_requests( $streams );
 
 $headers = consume_headers( $streams );
 foreach ( $streams as $k => $stream ) {
