de68e9c9491887f97b178ad5688d6f57f85d274b
Michael Reichardt <michal.reichardt@gmail.com>
2024-03-27 06:52:58 +0100

    Fix UnzipStepRunner PHP == 7.0 compatibility issue

--- src/WordPress/Blueprints/Runner/Step/UnzipStepRunner.php
+++ src/WordPress/Blueprints/Runner/Step/UnzipStepRunner.php
@@ -12,12 +12,12 @@ class UnzipStepRunner extends BaseStepRunner {
 	 * Runs the Unzip Step
 	 *
 	 * @param UnzipStep $input Step.
-	 * @param Tracker   $progress_tracker Tracker.
+	 * @param Tracker $progress_tracker Tracker.
 	 * @return void
 	 */
 	public function run(
-		$input,
-		$progress_tracker
+		UnzipStep $input,
+		Tracker   $progress_tracker
 	) {
 		$progress_tracker->set( 10, 'Unzipping...' );
 
